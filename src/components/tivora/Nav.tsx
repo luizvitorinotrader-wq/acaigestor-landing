a8b40e5d4f9433d3922bd6b4fd2ef01f63cb8443
luizvitorinotrader-wq <luizvitorinotrader@gmail.com>
2026-06-07 13:10:00 -0300

    Update Nav.tsx

--- src/components/tivora/Nav.tsx
+++ src/components/tivora/Nav.tsx
@@ -48,10 +48,10 @@ export function Nav() {
             aria-label="Tivora"
           >
             <img
-              src="/logo-tivora-dark.svg"
-              alt="Tivora"
-              className="h-9 sm:h-10 lg:h-11 w-auto object-contain transition-opacity duration-200 group-hover:opacity-90"
-            />
+  src="/logo-tivora.svg"
+  alt="Tivora"
+  className="h-10 w-auto"
+/>
           </a>
 
           {/* Desktop links */}
